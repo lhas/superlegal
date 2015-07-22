cb4346bc61811173718e60306238897e85e4ec78
lhas <luizhrqas@gmail.com>
2015-07-22 20:14:03 -0300

    atualização

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,24 +54,68 @@ module.exports = function(grunt) {
 		watch: {
 		  stylesheets: {
 		    files: 'src/**/*.styl',
-		    tasks: [ 'stylus' ]
+		    tasks: [ 'newer:stylus' ]
 		  },
 		  scripts: {
 		    files: 'src/**/*.js',
-		    tasks: [ 'uglify' ]
+		    tasks: [ 'newer:uglify' ]
 		  },
 		  jade: {
 		    files: 'src/**/*.jade',
-		    tasks: [ 'jade' ]
+		    tasks: [ 'newer:jade' ]
 		  },
 		  copy: {
-		    files: [ 'source/**', '!source/**/*.styl', '!source/**/*.coffee', '!source/**/*.jade' ],
+		    files: [ 'src/**', '!src/**/*.styl', '!src/**/*.coffee', '!src/**/*.jade' ],
 		    tasks: [ 'copy', 'clean:stylesheets', 'clean:scripts' ]
 		  },
 		  options: {
 		  	livereload: true,
 		  }
-		}
+		},
+		imagemin: {
+		    png: {
+		      options: {
+		        optimizationLevel: 7
+		      },
+		      files: [
+		        {
+		          // Set to true to enable the following options…
+		          expand: true,
+		          // cwd is 'current working directory'
+		          cwd: 'src/assets/img/',
+		          src: ['**/*.png'],
+		          // Could also match cwd line above. i.e. project-directory/img/
+		          dest: 'dist/assets/img/',
+		          ext: '.png'
+		        }
+		      ]
+		    },
+		    jpg: {
+		      options: {
+		        progressive: true
+		      },
+		      files: [
+		        {
+		          // Set to true to enable the following options…
+		          expand: true,
+		          // cwd is 'current working directory'
+		          cwd: 'src/assets/img/',
+		          src: ['**/*.jpg'],
+		          // Could also match cwd. i.e. project-directory/img/
+		          dest: 'dist/assets/img/',
+		          ext: '.jpg'
+		        }
+		      ]
+		    }
+	  },
+	  connect: {
+	    server: {
+	      options: {
+	        port: 3000,
+	        base: 'dist'
+	      }
+	    }
+	  }
 	});
 
   	grunt.loadNpmTasks('grunt-contrib-copy');
@@ -80,7 +124,10 @@ module.exports = function(grunt) {
   	grunt.loadNpmTasks('grunt-contrib-uglify');
   	grunt.loadNpmTasks('grunt-contrib-jade');
   	grunt.loadNpmTasks('grunt-contrib-watch');
+  	grunt.loadNpmTasks('grunt-contrib-imagemin');
+  	grunt.loadNpmTasks('grunt-newer');
+  	grunt.loadNpmTasks('grunt-contrib-connect');
 
-	grunt.registerTask("default", ['clean', 'copy', 'stylus', 'uglify', 'jade', 'clean:scripts', 'watch']);
-	grunt.registerTask("build", ['clean', 'copy', 'stylus', 'uglify', 'jade', 'clean:scripts']);
+	grunt.registerTask("default", ['clean', 'copy', 'stylus', 'uglify', 'jade', 'clean:scripts', 'imagemin', 'watch']);
+	grunt.registerTask("build", ['clean', 'copy', 'stylus', 'uglify', 'jade', 'clean:scripts', 'imagemin']);
 }
